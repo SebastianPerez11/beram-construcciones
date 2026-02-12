713d7ac1506df67c9734d83d2d0e95c2a2188f83
Sebastian Perez <sebasperez.dev@gmail.com>
2026-02-11 21:15:33 -0300

    update proyect

---
+++ src/data/proyectsData.js
@@ -0,0 +1,38 @@
+export const proyects =
+    [
+        {
+
+            titleProyect: "Refacción en departamento",
+            subtitle: "Transformamos un departamento antiguo en un espacio moderno y funcional.",
+            description: "Se realizaron trabajos de pintura, revoque fino y construcción en seco, priorizando terminaciones de alta calidad.",
+            location: "Boulevard Olimpico 4385.",
+            status: "COMPLETADO",
+            bgColor: "bg-red-300",
+        },
+        {
+
+            titleProyect: "Refacción de Casa",
+            subtitle: "Transformamos un departamento antiguo en un espacio moderno y funcional.",
+            description: "Se realizaron trabajos de pintura, revoque fino y construcción en seco, priorizando terminaciones de alta calidad.",
+            location: "Boulevard Olimpico 4385.",
+            status: "COMPLETADO",
+             bgColor: "bg-yellow-300",
+        }, { 
+
+            titleProyect: "Refacción en departamento",
+            subtitle: "Transformamos un departamento antiguo en un espacio moderno y funcional.",
+            description: "Se realizaron trabajos de pintura, revoque fino y construcción en seco, priorizando terminaciones de alta calidad.",
+            location: "Boulevard Olimpico 4385.",
+            status: "COMPLETADO",
+             bgColor: "bg-blue-300",
+        },
+         { 
+
+            titleProyect: "Refacción en departamento",
+            subtitle: "Transformamos un departamento antiguo en un espacio moderno y funcional.",
+            description: "Se realizaron trabajos de pintura, revoque fino y construcción en seco, priorizando terminaciones de alta calidad.",
+            location: "Boulevard Olimpico 4385.",
+            status: "COMPLETADO",
+             bgColor: "bg-green-300",
+        },
+    ]
---
+++ src/data/servicesData.js
@@ -0,0 +1,46 @@
+export const services = [
+    {
+        service: "Yesería",
+        description: "Trabajos de yeso, masillado, alisado de paredes y cielorrasos para terminaciones perfectas.",
+        features: ["Pintura interior y exterior", "Tratamiento de humedad", "Aplicación de texturas especiales", "Garantía de 2 años"],
+        measure: "320/m²",
+
+    },
+    {
+        service: "Pintura Profesional",
+        description: "Trabajos de yeso, masillado, alisado de paredes y cielorrasos para terminaciones perfectas.",
+        features: ["Pintura interior y exterior", "Tratamiento de humedad", "Aplicación de texturas especiales", "Garantía de 1 años"],
+        measure: "320/m²",
+
+    },
+    {
+        service: "Construcción en Seco (Durlock)",
+        description: "Trabajos de yeso, masillado, alisado de paredes y cielorrasos para terminaciones perfectas.",
+        features: ["Pintura interior y exterior", "Tratamiento de humedad", "Aplicación de texturas especiales", "Garantía de 2 años"],
+        measure: "320/m²",
+
+    },
+    {
+        service: "Colocación",
+        description: "Trabajos de yeso, masillado, alisado de paredes y cielorrasos para terminaciones perfectas.",
+        features: ["Pintura interior y exterior", "Tratamiento de humedad", "Aplicación de texturas especiales", "Garantía de 2 años"],
+        measure: "320/m²",
+
+    }
+    ,
+    {
+        service: "Plomería",
+        description: "Trabajos de yeso, masillado, alisado de paredes y cielorrasos para terminaciones perfectas.",
+        features: ["Pintura interior y exterior", "Tratamiento de humedad", "Aplicación de texturas especiales", "Garantía de 2 años"],
+        measure: "320/m²",
+
+    },
+     {
+        service: "Electricidad",
+        description: "Trabajos de yeso, masillado, alisado de paredes y cielorrasos para terminaciones perfectas.",
+        features: ["Pintura interior y exterior", "Tratamiento de humedad", "Aplicación de texturas especiales", "Garantía de 2 años"],
+        measure: "0/m²",
+
+    },
+
+]
